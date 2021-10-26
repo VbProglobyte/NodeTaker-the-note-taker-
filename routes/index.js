403cb483067db8a3d9632f6214cfa497c06d3fe6
Val Brisendine <vsbrisendine@gmail.com>
2021-10-26 00:56:40 -0500

    index router added

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 
-// Import our modular routers for /tips and /feedback
-const tipsRouter = require('./tips');
-const feedbackRouter = require('./feedback');
-const diagnosticsRouter = require('./diagnostics');
+// Import our modular routers for /notes and /feedback
+const notesRouter = require('./notes');
 
 const app = express();
 
-app.use('/tips', tipsRouter);
-app.use('/feedback', feedbackRouter);
-app.use('/diagnostics', diagnosticsRouter);
+app.use('/notes', notesRouter);
+
 
 module.exports = app;
