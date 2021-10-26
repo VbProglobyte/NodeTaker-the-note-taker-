8298f7ad6bf43333419d59aea655585d0a2e71e8
Val Brisendine <vsbrisendine@gmail.com>
2021-10-26 01:24:17 -0500

    working nodetaker hell yeauh!

--- routes/notes.js
+++ routes/notes.js
@@ -1,40 +1,40 @@
-const notes = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
 const {
   readFromFile,
   readAndAppend,
   writeToFile,
-} = require('../helpers/fsUtils');
+} = require("../helpers/fsUtils");
 
 // GET Route for retrieving all the notes
-notes.get('/', (req, res) => {
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 // GET Route for a specific note
-notes.get('/:id', (req, res) => {
+notes.get("/:id", (req, res) => {
   const noteId = req.params.id;
-  readFromFile('./db/db.json')
+  readFromFile("./db/db.json")
     .then((data) => JSON.parse(data))
     .then((json) => {
       const result = json.filter((note) => note.id === noteId);
       return result.length > 0
         ? res.json(result)
-        : res.json('No note with that ID');
+        : res.json("No note with that ID");
     });
 });
 
 // DELETE Route for a specific note
-notes.delete('/:id', (req, res) => {
+notes.delete("/:id", (req, res) => {
   const noteId = req.params.id;
-  readFromFile('./db/db.json')
+  readFromFile("./db/db.json")
     .then((data) => JSON.parse(data))
     .then((json) => {
       // Make a new array of all notes except the one with the ID provided in the URL
       const result = json.filter((note) => note.id !== noteId);
 
       // Save that array to the filesystem
-      writeToFile('./db/db.json', result);
+      writeToFile("./db/db.json", result);
 
       // Respond to the DELETE request
       res.json(`Item ${noteId} has been deleted 🗑️`);
@@ -42,10 +42,10 @@ notes.delete('/:id', (req, res) => {
 });
 
 // POST Route for a new UX/UI note
-notes.post('/', (req, res) => {
+notes.post("/", (req, res) => {
   console.log(req.body);
 
-  const { title, text} = req.body;
+  const { title, text } = req.body;
 
   if (req.body) {
     const newnote = {
@@ -54,10 +54,10 @@ notes.post('/', (req, res) => {
       id: uuidv4(),
     };
 
-    readAndAppend(newnote, './db/db.json');
+    readAndAppend(newnote, "./db/db.json");
     res.json(`note added successfully 🚀`);
   } else {
-    res.error('Error in adding note');
+    res.error("Error in adding note");
   }
 });
 
