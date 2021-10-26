1e473f018d7d113246e848fa7978f8f7c34e6f8c
Val Brisendine <vsbrisendine@gmail.com>
2021-10-26 00:54:37 -0500

    adding structure

--- routes/notes.js
+++ routes/notes.js
@@ -12,12 +12,12 @@ notes.get('/', (req, res) => {
 });
 
 // GET Route for a specific note
-notes.get('/:note_id', (req, res) => {
-  const noteId = req.params.note_id;
+notes.get('/:id', (req, res) => {
+  const noteId = req.params.id;
   readFromFile('./db/db.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
-      const result = json.filter((note) => note.note_id === noteId);
+      const result = json.filter((note) => note.id === noteId);
       return result.length > 0
         ? res.json(result)
         : res.json('No note with that ID');
@@ -25,13 +25,13 @@ notes.get('/:note_id', (req, res) => {
 });
 
 // DELETE Route for a specific note
-notes.delete('/:note_id', (req, res) => {
-  const noteId = req.params.note_id;
+notes.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
   readFromFile('./db/db.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
       // Make a new array of all notes except the one with the ID provided in the URL
-      const result = json.filter((note) => note.note_id !== noteId);
+      const result = json.filter((note) => note.id !== noteId);
 
       // Save that array to the filesystem
       writeToFile('./db/db.json', result);
@@ -45,14 +45,13 @@ notes.delete('/:note_id', (req, res) => {
 notes.post('/', (req, res) => {
   console.log(req.body);
 
-  const { username, topic, note } = req.body;
+  const { title, text} = req.body;
 
   if (req.body) {
     const newnote = {
-      username,
-      note,
-      topic,
-      note_id: uuidv4(),
+      title,
+      text,
+      id: uuidv4(),
     };
 
     readAndAppend(newnote, './db/db.json');
